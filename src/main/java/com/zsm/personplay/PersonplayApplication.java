85d1d049a01c40881afe728411a2b572363543e2
zzlzlizhen <275290501@qq.com>
2019-12-24 17:51:23 +0800

    spingboot项目第一版提交

---
+++ src/main/java/com/zsm/personplay/PersonplayApplication.java
@@ -0,0 +1,11 @@
+package com.zsm.personplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonplayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PersonplayApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zsm/personplay/PersonplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsm.personplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
